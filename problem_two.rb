901e3d33e3913560d53512e31f16ce6c62fcdf5f
mgmilton <matt.milton@gmail.com>
2018-05-30 09:17:50 -0600

    adds problem two

---
+++ problem_two.rb
@@ -0,0 +1,7 @@
+# Problem 2
+# Write a function that combines two lists by alternatingly taking elements. For example: given the two lists [a, b, c] and [1, 2, 3], the function should return [a, 1, b, 2, c, 3].
+
+
+def alternates(array_1, array_2)
+
+end

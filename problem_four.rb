ec3b3b68cd1f128b3fbf70895fec1f46c491dac3
mgmilton <matt.milton@gmail.com>
2018-05-30 09:33:56 -0600

    adds problem four

--- problem_four.rb
+++ problem_four.rb
@@ -1,2 +1,10 @@
 # Problem 4
 # Write a function that given a list of non negative integers, arranges them such that they form the largest possible number. For example, given [50, 2, 1, 9], the largest formed number is 95021.
+
+require "pry"
+def rearranger(array)
+  binding.pry
+end
+
+
+puts rearranger([50, 2, 1, 9])

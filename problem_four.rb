26e0760f9a03ed88827b58432a2a2c73d8ca0d0a
mgmilton <matt.milton@gmail.com>
2018-05-30 10:12:38 -0600

    adds problem four

--- problem_four.rb
+++ problem_four.rb
@@ -1,20 +1,13 @@
 # Problem 4
 # Write a function that given a list of non negative integers, arranges them such that they form the largest possible number. For example, given [50, 2, 1, 9], the largest formed number is 95021.
 
-require "pry"
 def rearranger(array)
   characters = array.map(&:to_s)
-  num = ""
-  temp = []
-  characters.each_with_index do |character, index|
-    if character.length > 1
-      temp << character[0].to_i
-    else
-      temp << array[index]
-    end
-  end
-  binding.pry
+  characters.sort! {|x, y| y[0] <=> x[0]}
+  characters.sort!.reverse!
+  return characters.join("").to_i
 end
 
 
-puts rearranger([50, 2, 1, 9])
+
+puts rearranger([50, 2, 1, 9, 53, 100, 120])

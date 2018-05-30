7a7f2ac1337af92d8e0ea728da011629c141bd3d
mgmilton <matt.milton@gmail.com>
2018-05-30 09:27:18 -0600

    adds problem two

--- problem_two.rb
+++ problem_two.rb
@@ -1,14 +1,17 @@
 # Problem 2
 # Write a function that combines two lists by alternatingly taking elements. For example: given the two lists [a, b, c] and [1, 2, 3], the function should return [a, 1, b, 2, c, 3].
-require 'pry'
+require 'pry';
 
 def alternates(array_1, array_2)
   temp = []
-  array_1.each_with_inded do |value, index|
-    binding.pry
-    temp << array_1[index]
-  end
+  count = 0
 
+  while count <= array_1.length - 1 do
+    temp << array_1[count]
+    temp << array_2[count]
+    count += 1
+  end
+  print temp
 end
 
-alternates([a, b, c], [1, 2, 3] )
+alternates( ["a", "b", "c"], [1, 2, 3])
